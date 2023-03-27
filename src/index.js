7993ee189a127f3e1b8f94468dbff3ca9933e043
Gabriela Muniz <gabrielanamu@live.com>
2023-03-27 11:06:15 -0300

    change broeserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -12,11 +12,11 @@ import './pages/css/index.css';
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={ store }>
         <App />
       </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
   );
 
 // If you want your app to work offline and load faster, you can change
